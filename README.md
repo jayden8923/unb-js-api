024f5406a07c73856e741b99e8414a6f506f1d2e
Zₐₗgₒ <logang4lyfe1@gmail.com>
2020-06-26 12:53:48 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# jayden8923/unb-js-api
+---
+This is my version of unb-api. 
+
+### Example Usage:
+```js
+const unb-js-api = require('jayden8923/unb-js-api'); 
+const apiclient = new unb-js-api('api-token');
+```
---
+++ src/client.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+module.exports = class {
+  constructor(apitoken, version) {
+    if (!apitoken) throw new Error('No api token provided');
+    if (typeof apitoken !== 'string') throw new TypeError('Api token must be a string');
+   this.token = apitoken;
+  if (!version || typeof version !== 'number') this.version = "v1";
+  else this.version = 'v' + version;
+  };
+
+   const instance = axios.create({
+    baseURL: 'https://unbelievaboat.com/api/' + this.version,
+    timeout: 1000,
+    headers: {'Authorization': this.token}
+});
+}
