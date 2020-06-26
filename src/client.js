7e13b72002f0c9bd6c336542facded402a8d9249
Zₐₗgₒ <logang4lyfe1@gmail.com>
2020-06-26 13:23:20 +0000

    gitignore; minor changes

--- src/client.js
+++ src/client.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 module.exports = class {
   constructor(apitoken, version) {
-    if (!apitoken) throw new Error('No api token provided');
-    if (typeof apitoken !== 'string') throw new TypeError('Api token must be a string');
+    if (!apitoken) throw new Error('No API token provided');
+    if (typeof apitoken !== 'string') throw new TypeError('API token must be a string');
    this.token = apitoken;
   if (!version || typeof version !== 'number') this.version = "v1";
   else this.version = 'v' + version;
@@ -13,4 +13,17 @@ module.exports = class {
     timeout: 1000,
     headers: {'Authorization': this.token}
 });
+
+/**
+ * Get a user's balance.
+ * @param {string} guildid
+ * @param {string} userid
+ */
+this.getUserBalance = function(guildid, userid) {
+  if (!guildid) throw new Error('No guild ID provided');
+  else if (!userid) throw new Error('No user ID provided');
+  else if (typeof guildid !== 'string') throw new TypeError('Guild ID must be a string');
+  else if (typeof userid !== 'string') throw new TypeError('User ID must be a string');
+return instance.get(`/guilds/{guild.id}/users/{user.id}`);
+}
 };
